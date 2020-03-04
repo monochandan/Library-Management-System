36dec1e0b321a2448444c0704e9b9ed1d0bdef3d
monochandan <monochandan95@gmail.com>
2020-03-04 12:46:17 +0600

    LSM

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2017 Online Library Management System |<a href=" "  > Designed by : Mono Chandan</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
